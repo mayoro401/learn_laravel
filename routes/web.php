93a382ae4ffdb2bb480523256abfcc2ba76a11ae
mayoro401 <mayoro.fall@uadb.edu.sn>
2023-11-13 09:46:31 +0000

    les routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
-
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Accueil;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,49 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/user', function(){
+    return 'Bye user';
+});
+
+Route::get('/admin', function(){
+    return view('admin');
+});
+
+
+Route::get('/tableauparam', function(){
+    return [
+        "name" => $_GET['name'],
+        "description"=>"etudiant"
+    ];
+});
+Route::get('/tableau', function(Request $request){
+    return [
+        // "name" => $request -> path(),       
+        // "name" => $request -> url(),
+        "name" => $request -> all(),
+        "description"=>"etudiant"
+    ];
+});
+
+Route::get('/user/{produits}-{id}', function($id){
+    return [
+        "id" => $id,
+    ];
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        // Correspond à l'URL /admin/dashboard
+        return 'bienvenue dans le dashboard ';
+    });
+    Route::get('/utilisateurs', function () {
+        // Correspond à l'URL /admin/utilisateurs
+        return 'bienvenue dans le utilisateurs ';
+    });
+});
+
+//route avec controller
+Route::get('/accueil', [Accueil::class, 'index'])->name('accueil');
+
